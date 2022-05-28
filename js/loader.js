065aba0f50976a277580153caaaeabf43e2bf766
adrianBrands <adrianbrandshaug@hotmail.com>
2022-05-28 22:44:56 +0200

    workin on loader

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
 function stopLoader (){
     console.log("loaded");
-    loader.style.display="none";
+    /*loader.style.display="none";*/
   }
 
   const loader = document.querySelector(".loader");
- /* window.onload = stopLoader();*/
+  window.onload = stopLoader();
